b8a0a19ec11f57f39a2eef9d6bcd1b57e5d39497
CHENSE5 <chense5@oocl.com>
2024-12-02 11:21:12 +0800

    init

---
+++ src/components/Recommend.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-4">
+    <el-card style="width: 480px" shadow="always">Always</el-card>
+    <el-card style="width: 480px" shadow="hover">Hover</el-card>
+    <el-card style="width: 480px" shadow="never">Never</el-card>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
